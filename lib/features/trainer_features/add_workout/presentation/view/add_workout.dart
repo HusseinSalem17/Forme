0a546f8e488f2f64505e3a75d8b0451642b59b7c
Ahmedramy2000 <ahmed.ramy253545@gmail.com>
2024-07-04 04:39:16 +0300

    solve andriod issue

--- lib/features/trainer_features/add_workout/presentation/view/add_workout.dart
+++ lib/features/trainer_features/add_workout/presentation/view/add_workout.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-
 import 'package:forme_app/core/utils/app_colors.dart';
 import 'package:forme_app/core/utils/text_styles.dart';
 import 'package:forme_app/core/widgets/custom_app_bar_arrow_button.dart';
-import 'package:forme_app/core/widgets/image_picker/profile_image_picker.dart';
 import 'package:forme_app/core/widgets/import_media.dart';
 import 'package:forme_app/features/trainer_features/add_workout/data/models/workout_details.dart';
 import 'package:forme_app/features/trainer_features/add_workout/presentation/manager/bloc/work_out_bloc.dart';
